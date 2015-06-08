ef2be11b13ec6cabb8c82495c82395d1d676ed2f
Kevin Amorim <kevinamorim4@gmail.com>
2015-06-08 09:14:30 +0100

    Cleaned useless files

--- TrafficarWebClient/content/js/app.js
+++
@@ -1,5 +0,0 @@
-function validateRegistration() {
-	document.register.hash.value = CryptoJS.SHA3(document.register.password.value);
-	document.register.password.value = document.register.hash.value;
-	document.register.submit();
-}
